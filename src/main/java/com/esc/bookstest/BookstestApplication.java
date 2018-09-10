b0ada50b43c98b4bbd37ff58eea09a9f6651e8fe
vova <Volodymyyrr@gmail.com>
2018-09-10 18:18:23 +0300

    Created skeleton project

---
+++ src/main/java/com/esc/bookstest/BookstestApplication.java
@@ -0,0 +1,12 @@
+package com.esc.bookstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstestApplication.class, args);
+	}
+}
